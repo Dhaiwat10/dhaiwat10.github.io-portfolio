39dc934e5901430494e4d5427d5dd64b706381db
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-03-14 18:03:46 +0530

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+
+    <h1 className="name">DHAIWAT PANDYA</h1>
+
+    <div className="tagline">
+      full-stack software engineer
+    </div>
+
+    <button className="view_my_work_btn">
+      VIEW MY WORK
+    </button>
+  </div>
+)

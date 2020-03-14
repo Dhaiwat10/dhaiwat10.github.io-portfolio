b17b06c9686dcb69c271d3339b3f322391f3f341
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-03-14 19:20:06 +0530

    Tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 
-export default () => (
-  <div>
+export default() => (
+    <div>
+        <div className="landing">
+            <h1 className="name">DHAIWAT PANDYA</h1>
 
-    <h1 className="name">DHAIWAT PANDYA</h1>
+            <div className="tagline">
+                FULL-STACK SOFTWARE ENGINEER
+            </div>
 
-    <div className="tagline">
-      full-stack software engineer
+            <button className="view_my_work_btn">
+                VIEW MY WORK
+            </button>
+        </div>
+
     </div>
-
-    <button className="view_my_work_btn">
-      VIEW MY WORK
-    </button>
-  </div>
 )
